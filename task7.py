72ec4a715668d3ad61b26c4308d1ef005e1446f0
Марченко Софья Константиновна <s.marchenko1@main.nsu.ru>
2024-10-07 10:03:39 +0700

    Лесс 3

--- task7.py
+++ task7.py
@@ -5,3 +5,25 @@
 # которая начинается с двух единиц и каждое следующее число
 # равно сумме двух предыдущих: 1, 1, 2, 3, 5, 8, 13, …
 
+
+s = []
+d = []
+while (a := int(input())) != 0:
+    s.append(a)
+if s:
+    s.sort()
+    i = 0
+    a = b = 1
+    while b <= max(s):
+        d.append(a)
+        d.append(b)
+        b += a
+        a += b
+    for i in s:
+        if i in d:
+            print(f'Минимальное число Фибоначчи: {i}')
+            break
+    else:
+        print('нет')
+else:
+    print('нет')
